f5bf9259639a9b08cd8539f14a38cb2f2d96cfc8
Enrique García <enrique.garcia@redpontis.com>
2024-07-20 23:22:22 -0600

    refactor: 💡 api para llamar al metodo del login
    agrego la parte controladora para llamar al metodo para que pueda
    realizar el login de la aplicacion

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { LoginDto } from './dto/authRequest.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService){}
+
+    @Post('login')
+    login(@Body() user: LoginDto){
+        return this.authService.login(user.email, user.password);
+    }
+}
